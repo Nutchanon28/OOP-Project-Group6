a6ef469fca31fac2488c9cf5288681cba0d0133e
Nutchanon28 <65010290@kmitl.ac.th>
2023-03-20 23:34:59 +0700

    <feat> add methods to class Project

--- project.py
+++ project.py
@@ -9,7 +9,6 @@ class Project:
         self.__pledge_backers = pledge_backers
         self.__pledge_duration = pledge_duration
         self.__category = category
-        # self.__filters = filters # remove this attribute
         self.__pledge_rewards = pledge_rewards
         self.__risk_detail = risk_detail
         self.__question = question
@@ -25,8 +24,43 @@ class Project:
         self.__pleadge_rewards = []
         self.__pleadge_backers = []
         self.__update = []
-        # self.__community = Community
 
+    def get_project_detail(self):
+        pass
+
+    def get_creator_detail(self):
+        pass
+
+    def edit_project(self, project_name, category, project_image, pledge_duration):
+        pass
+
+    def set_reward_shipping(self, estimated_delivery, ships_to, old_reward_shipping):
+        pass
+
+    def set_pledge_rewards(self, reward_goal, reward_name, reward_detail, max_reward_backers, reward_include, old_pledge_reward):
+        pass
+
+    def verify_creator_detail(self, legal_first_name, legal_last_name, email_address, date_of_birth, home_address, city, state, postal_code, phone_number):
+        pass
+
+    def create_comment(self, sending_time, text, writer):
+        pass
+
+    def create_update(self, update_title, update_creator, update_detail, update_image):
+        pass
+
+    @property
+    def pledge_reward(self):
+        return self.__pleadge_rewards
+    
+    @property
+    def pledge_goal(self):
+        return self.__pledge_goal
+    
+    @pledge_goal.setter
+    def pledge_goal(self, new_pledge_goal):
+        if isinstance(new_pledge_goal, int):
+            self.__pledge_goal
     @property
     def project_name(self):
         return self.__project_name
