9314616624303ddd6bc1f5480d4a58e6f6ace28f
Thananya Cemarkharn <65010434@kmitl.ac.th>
2023-03-20 23:52:59 +0700

    <feat> add class Update

---
+++ update.py
@@ -0,0 +1,7 @@
+class Update:
+    def __init__(self, update_title, update_creator, update_detail, update_image, likes):
+        self.__update_title = update_title
+        self.__update_creator = update_creator
+        self.__update_detail = update_detail
+        self.__update_image = update_image
+        self.__likes = likes
