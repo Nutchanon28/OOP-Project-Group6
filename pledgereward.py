12b6c91cbc75b4ec9ccd38dfaf4e78299f10e052
Thananya Cemarkharn <65010434@kmitl.ac.th>
2023-03-20 23:54:55 +0700

    <feat> add class PledgeReward

---
+++ pledgereward.py
@@ -0,0 +1,8 @@
+class PledgeReward:
+    def __init__(self, reward_goal, reward_name, reward_detail, reward_include, reward_backers, max_reward_backers):
+        self.__reward_goal = reward_goal
+        self.__reward_name = reward_name
+        self.__reward_detail = reward_detail
+        self.__reward_include = reward_include
+        self.__reward_backers = reward_backers
+        self.__max_reward_backers = max_reward_backers
