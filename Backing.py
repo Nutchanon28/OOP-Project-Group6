b6ec53291f6c7866f4fd9a4a2b536213a4e326c0
Jetsarit Kaewsaart <applerew14112546@gmail.com>
2023-03-21 17:27:14 +0700

    <feat> add class  Backing

---
+++ Backing.py
@@ -0,0 +1,9 @@
+class Backing:
+    def __init__(self, backer, project, reward_item, reward_cost, bonus_cost):
+        self.__backer = backer
+        self._project = project
+        self.__reward_item = reward_item
+        self.__reward_cost = reward_cost
+        self.__bonus_cost = bonus_cost
+
+    
