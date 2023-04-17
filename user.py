26e78a06751370ef4a021a0de6a2965fb98b4125
janipang <pang.janista@gmail.com>
2023-04-17 14:09:09 +0700

    feat : func in update,user,project

--- user.py
+++ user.py
@@ -1,41 +1,72 @@
+from backing import Backing
+from address import Address
+from comment import Comment
 class User:
-    def __init__(self, account, message, payment_method, name, avatar, biography, location, time_zone, vanity_url, website, addresses, following, followers, blocked, notification):
-        self.__account = account
-        self.__message = message
-        self.__payment_method = payment_method
+    id_counter = 1
+    
+    def __init__(self, id,  gmail, password, name, avatar, biography, location, website):
+        self.id = User.id_counter
+        User.id_counter += 1
+        
+        self.__id = id
+        self.__gmail = gmail
+        self.__password = password
+        self.__payment_method = [] #list of creditcard
         self.__name = name
         self.__avatar = avatar
         self.__biography = biography
         self.__location = location
-        self.__time_zone = time_zone
+        self.__website = website
+        self.__addresses = []
+        self.__notifications = []
+        self.__backings = []
+    
+    def edit_user(self, gmail, password, name, avatar, biography, location, vanity_url, website):
+        self.__gmail = gmail
+        self.__password = password
+        self.__name = name
+        self.__avatar = avatar
+        self.__biography = biography
+        self.__location = location
         self.__vanity_url = vanity_url
         self.__website = website
-        self.__addresses = addresses
-        self.__following = following
-        self.__followers = followers
-        self.__blocked = blocked
-        self.__notification = notification
         
+    def back_project(self, project, credit_card, pledge_reward, bonus_cost):
+        # if make_payment(credit_card, pledge_reward.reward_goal) == "successful":
+        if True:
+            print(f"lose {pledge_reward.reward_goal} bath from {credit_card}")
+            new_backing = Backing(self.name, project, pledge_reward, bonus_cost)
+            self.__backings.append(new_backing)
+            project.add_backing(new_backing)
+            print("success backing")
+        else:
+            print("insufficient fund")
+        
+    def add_address(self, country, address_nickname, full_name, address, city, phone_number):
+        new_address = Address(country, address_nickname, full_name, address, city, phone_number)
+        self.__addresses.append(new_address)
+        return "finished add address"
+
+    def create_address(self, country, address_nickname, full_name, address, city, phone_number):
+        self.__address_list.append(Address(country, address_nickname, full_name, address, city, phone_number))
+                
+    def send_comment(self, project, ):
+        self.__name
+        new_comment = Comment("time_now", "text", self)
+              
+    def add_payment_method(self):
+        pass
+    def save_credit_card(self):
+        pass
+    
     @property
     def notification(self):
         return self.__notification
     
     @property
-    def following(self):
-        return self.__following
-    
-    @property
-    def followers(self):
-        return self.__followers
-    
-    @property
-    def blocked(self):
-        return self.__blocked
-    
-    @property
     def addresses(self):
         return self.__addresses
     
-    def add_address(self, country, address_nickname, full_name, address, city, phone_number):
-        pass
-
+    @property
+    def id(self):
+        return self.__id
